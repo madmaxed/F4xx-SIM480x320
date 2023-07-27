b7dfe3cef74e52e6dc2f931dcc7889b0fa8474f7
Jon Grimshaw <jrg@3dxtech.com>
2023-07-27 19:50:56 -0400

    Fix new PlatformIO format

--- buildroot/share/PlatformIO/scripts/marlin.py
+++ buildroot/share/PlatformIO/scripts/marlin.py
@@ -15,12 +15,17 @@ def copytree(src, dst, symlinks=False, ignore=None):
 		else:
 			shutil.copy2(item, dst / item.name)
 
+#def replace_define(field, value):
+#	for define in env['CPPDEFINES']:
+##			env['CPPDEFINES'].remove(define)
+#	env['CPPDEFINES'].append((field, value))
 def replace_define(field, value):
-	for define in env['CPPDEFINES']:
-		if define[0] == field:
-			env['CPPDEFINES'].remove(define)
-	env['CPPDEFINES'].append((field, value))
-
+    envdefs = env['CPPDEFINES'].copy()
+    for define in envdefs:
+        if define[0] == field:
+            env['CPPDEFINES'].remove(define)
+    env['CPPDEFINES'].append((field, value))
+    
 # Relocate the firmware to a new address, such as "0x08005000"
 def relocate_firmware(address):
 	replace_define("VECT_TAB_ADDR", address)
